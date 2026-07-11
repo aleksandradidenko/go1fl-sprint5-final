926ae12dfcc49f25dbf001037db037214b195d65
Aleksandra Didenko <didenko0a@yandex.ru>
2026-07-12 02:11:57 +0300

    implement spent energy calculations

--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -25,5 +25,8 @@ func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
 }
 
 func Distance(steps int, height float64) float64 {
-	// TODO: реализовать функцию
+	stepLength := height * stepLengthCoefficient
+	distance := float64(steps) * stepLength / mInKm
+
+	return distance
 }
