50c82d23bff848d880834b1c0e5015b359ca9dfd
Aleksandra Didenko <didenko0a@yandex.ru>
2026-07-12 02:53:18 +0300

    implement training parsing

--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -2,11 +2,13 @@ package trainings
 
 import (
 	"errors"
+	"fmt"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
 )
 
 type Training struct {
@@ -47,5 +49,47 @@ func (t *Training) Parse(datastring string) (err error) {
 }
 
 func (t Training) ActionInfo() (string, error) {
+	distance := spentenergy.Distance(t.Steps, t.Height)
+	meanSpeed := spentenergy.MeanSpeed(t.Steps, t.Height, t.Duration)
 
+	var spentCalories float64
+	var err error
+
+	switch t.TrainingType {
+	case "Бег":
+		spentCalories, err = spentenergy.RunningSpentCalories(
+			t.Steps,
+			t.Weight,
+			t.Height,
+			t.Duration,
+		)
+	case "Ходьба":
+		spentCalories, err = spentenergy.WalkingSpentCalories(
+			t.Steps,
+			t.Weight,
+			t.Height,
+			t.Duration,
+		)
+	default:
+		return "", errors.New("неизвестный тип тренировки")
+	}
+
+	if err != nil {
+		return "", err
+	}
+
+	info := fmt.Sprintf(
+		"Тип тренировки: %s\n"+
+			"Длительность: %.2f ч.\n"+
+			"Дистанция: %.2f км.\n"+
+			"Скорость: %.2f км/ч\n"+
+			"Сожгли калорий: %.2f\n",
+		t.TrainingType,
+		t.Duration.Hours(),
+		distance,
+		meanSpeed,
+		spentCalories,
+	)
+
+	return info, nil
 }
